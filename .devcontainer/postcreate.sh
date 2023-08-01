3a8ce72aef64e1fa5ca8d7c99e5eac512343444f
itzandroidtab <9889898+itzandroidtab@users.noreply.github.com>
2023-08-01 20:23:52 +0000

    moved arm-none-eabi out of project

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,8 +1,8 @@
-mkdir arm-none-eabi/
-cd arm-none-eabi/
+mkdir ../arm-none-eabi/
+cd ../arm-none-eabi/
 
 wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
-tar xvf arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
+tar xf arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 
 sudo rm -f /usr/bin/arm-none-eabi-gcc 
 sudo rm -f /usr/bin/arm-none-eabi-g++
@@ -11,9 +11,9 @@ sudo rm -f /usr/bin/arm-none-eabi-size
 sudo rm -f /usr/bin/arm-none-eabi-objcopy
 sudo rm -f /usr/bin/arm-none-eabi-objdump
 
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
-sudo ln -s arm-none-eabi/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
+sudo ln -s arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
