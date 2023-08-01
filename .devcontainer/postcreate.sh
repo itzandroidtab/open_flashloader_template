ef56535d0aacc60fa3325a3b403366854b30045b
itzandroidtab <9889898+itzandroidtab@users.noreply.github.com>
2023-08-01 20:43:40 +0000

    added check to prevent downloading 2x

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,7 +1,13 @@
 mkdir ../arm-none-eabi/
 cd ../arm-none-eabi/
 
-wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
+if [ ! -f ./arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz ]
+then
+    echo "Downloading arm-none-eabi-gcc"
+    wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
+fi
+
+echo "Unpacking arm-none-eabi-gcc"
 tar xf arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 
 sudo rm -f /usr/bin/arm-none-eabi-gcc || true
@@ -10,6 +16,8 @@ sudo rm -f /usr/bin/arm-none-eabi-gdb || true
 sudo rm -f /usr/bin/arm-none-eabi-size || true
 sudo rm -f /usr/bin/arm-none-eabi-objcopy || true
 sudo rm -f /usr/bin/arm-none-eabi-objdump || true
+
+echo "Linking arm-none-eabi"
 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
