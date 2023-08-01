73901566f7dd55f79cd68b317d520cb4246fc837
itzandroidtab <9889898+itzandroidtab@users.noreply.github.com>
2023-08-01 20:37:23 +0000

    made it continue on failing to remove

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,12 +4,12 @@ cd ../arm-none-eabi/
 wget https://developer.arm.com/-/media/Files/downloads/gnu/12.3.rel1/binrel/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 tar xf arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi.tar.xz
 
-sudo rm -f /usr/bin/arm-none-eabi-gcc 
-sudo rm -f /usr/bin/arm-none-eabi-g++
-sudo rm -f /usr/bin/arm-none-eabi-gdb
-sudo rm -f /usr/bin/arm-none-eabi-size
-sudo rm -f /usr/bin/arm-none-eabi-objcopy
-sudo rm -f /usr/bin/arm-none-eabi-objdump
+sudo rm -f /usr/bin/arm-none-eabi-gcc || true
+sudo rm -f /usr/bin/arm-none-eabi-g++ || true
+sudo rm -f /usr/bin/arm-none-eabi-gdb || true
+sudo rm -f /usr/bin/arm-none-eabi-size || true
+sudo rm -f /usr/bin/arm-none-eabi-objcopy || true
+sudo rm -f /usr/bin/arm-none-eabi-objdump || true
 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc 
 sudo ln -s `pwd`/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
